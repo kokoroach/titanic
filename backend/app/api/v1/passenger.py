91fbe1bfe786d380d93a54e890939d2dc364ae10
kokoroach <jonel.getigan@gmail.com>
2025-11-04 22:33:38 +0800

    refactor: separate redis logic

--- backend/app/api/v1/passenger.py
+++ backend/app/api/v1/passenger.py
@@ -2,7 +2,7 @@ import json
 from csv import DictReader
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from io import StringIO
-from typing import List
+from typing import List, Dict
 
 from app.core.logging import logger
 from app.db.passenger import (
@@ -10,7 +10,13 @@ from app.db.passenger import (
     get_all_passengers,
     get_passenger_by_id
 )
-from app.core.cache import get_redis, delete_keys_with_prefix, CACHE_TTL
+from app.core.cache import (
+    get_redis,
+    get_data_from_cache,
+    delete_keys_having_prefix,
+    set_cache_data
+)
+
 
 from app.domain.exceptions import DataValidationError
 from app.domain.passenger import Passenger
@@ -20,6 +26,14 @@ PASSENGER_PREFIX = "passengers:"
 
 
 router = APIRouter()
+
+
+# Cache Interfaces
+
+
+async def _invalidate_all_passenger_cache() -> None:
+    await delete_keys_having_prefix(prefix=PASSENGER_PREFIX)
+
 
 
 @router.post("/upload-csv", status_code=200)
@@ -39,65 +53,62 @@ async def upload_csv(file: UploadFile = File(...)):
     reader = DictReader(buffer)
 
     valid_passengers = []
-    invalid_passenger = []
+    invalid_passengers = []
     for row in reader:
         try:
+            # Inline data validation
             p = await Passenger.from_dict(row)
             valid_passengers.append(p.as_json())
         except DataValidationError:
-            invalid_passenger.append(p)
+            invalid_passengers.append(p)
 
+    # Bulk insertion to database
     try:
         await bulk_insert_passengers(valid_passengers)
     except Exception as e:
         logger.error(f"API: Ran an issue when inserting passengers: {e}")
     finally:
-        if invalid_passenger:
+        if invalid_passengers:
             logger.warning(
                 "Found invalid/malformed passenger data: "
-                f"{invalid_passenger}"
+                f"{invalid_passengers}"
             )
 
-    # Invalidate all existing passenger cache
-    redis = await get_redis()
-    await delete_keys_with_prefix(redis=redis, prefix=PASSENGER_PREFIX)
-
+    # Reset cache
+    await _invalidate_all_passenger_cache()
 
 @router.get("/all", status_code=200)
 async def get_passengers() -> List[dict]:
     """Return all of the Titanic passenger data"""
     redis_key = f"{PASSENGER_PREFIX}:all"
-    redis = await get_redis()
 
-    # Check cache
-    cached = await redis.get(redis_key)
-    if cached:
-        logger.info("Getting all passengers from cache.")
-        return json.loads(cached)
+    cached_data = await get_data_from_cache(redis_key)
+    if cached_data:
+        return cached_data
 
     _passengers = await get_all_passengers()
-    passengers = [p.to_dict() for p in _passengers]
+    passengers: List[Dict] = [p.to_dict() for p in _passengers]
 
-    # Build cache
-    await redis.set(redis_key, json.dumps(passengers), ex=CACHE_TTL)
+    # Set cache data
+    await set_cache_data(redis_key, passengers)
+
+    return passengers
 
 
 @router.get("/{p_id}", status_code=200)
 async def get_passenger(p_id: int):
     """Return a Titanic passenger"""
     redis_key = f"{PASSENGER_PREFIX}:{p_id}"
-    redis = await get_redis()
 
     # Check cache
-    cached = await redis.get(redis_key)
-    if cached:
-        logger.info("Getting all passengers from cache.")
-        return json.loads(cached)
+    cached_data = await get_data_from_cache(redis_key)
+    if cached_data:
+        return cached_data
 
     passenger = await get_passenger_by_id(p_id)
     p = passenger.to_dict()
 
-    # Build cache
-    await redis.set(redis_key, json.dumps(p), ex=CACHE_TTL)
+    # Set cache data
+    await set_cache_data(redis_key, p)
 
     return p
--- backend/app/core/cache.py
+++ backend/app/core/cache.py
@@ -1,6 +1,11 @@
+import json
+from typing import List, Any
+
 import redis.asyncio as _redis
 from redis.asyncio.client import Redis
+
 from app.core.logging import logger
+
 
 REDIS_URL = "redis://localhost:6379"
 CACHE_TTL = 600  # 10 mins
@@ -27,9 +32,25 @@ async def close_redis():
     await redis.close()
 
 
-async def delete_keys_with_prefix(redis: Redis, prefix: str):
+async def delete_keys_having_prefix(prefix: str):
+    redis = await get_redis()
     cursor = b'0'
     while cursor:
-        cursor, keys = await redis.scan(cursor=cursor, match=f"{prefix}*", count=100)
+        cursor, keys = await redis.scan(
+            cursor=cursor,
+            match=f"{prefix}*",
+            count=100
+        )
         if keys:
             await redis.delete(*keys)
+
+
+async def get_data_from_cache(key: str) -> dict | None:
+    redis = await get_redis()
+    cached = await redis.get(key)
+    return json.loads(cached) if cached else None
+
+
+async def set_cache_data(key: str, data: Any) -> None:
+    redis = await get_redis()
+    await redis.set(key, json.dumps(data), ex=CACHE_TTL)
