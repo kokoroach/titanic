50a61f35211f51686cd0832fc5b7640992a9a2c2
kokoroach <jonel.getigan@gmail.com>
2025-11-05 03:36:51 +0800

    feat: dockerize react

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,8 +1,11 @@
+import os
+
 # Database
 DATABASE_URL = "sqlite+aiosqlite:///titanic.db"
 
 # Redis
-REDIS_URL = "redis://localhost:6379"
+# NOTE: uses docker-compose REDIS_HOST
+REDIS_URL = f"redis://{os.getenv('REDIS_HOST', 'localhost')}:6379"
 REDIS_CACHE_TTL = 600  # 10 mins
 
 # CORS
