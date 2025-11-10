27ed2c3178691b79212304cc00403bbd4a09af51
kokoroach <jonel.getigan@gmail.com>
2025-11-10 15:03:01 +0800

    fix: routers

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -4,10 +4,9 @@ from app.api.v1.routes import local, passengers, stats
 from app.core.config import settings
 
 api_router = APIRouter()
-api_router.include_router(passengers.router)
-api_router.include_router(stats.router)
-api_router.include_router(local.router)
+api_router.include_router(passengers.router, prefix="/passengers")
+api_router.include_router(stats.router, prefix="/stats")
 
 
 if settings.ENVIRONMENT == "local":
-    api_router.include_router(local.router)
+    api_router.include_router(local.router, prefix="/local")
